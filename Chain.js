c4c6d94e2e4722676980580bafdc02a33ae7e78d
SuchetaChakraborty <73585914+SuchetaChakraborty@users.noreply.github.com>
2020-12-16 19:41:55 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,17 @@
+class Chain{
+    constructor(firstBody,secondBody){
+        var options ={
+           bodyA: firstBody,
+           bodyB: secondBody,
+           stiffness: 0.05,
+           length: 10   
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+      line(this.chain.bodyA.position.x, this.chain.bodyA.position.y, this.chain.bodyB.position.x, this.chain.bodyB.position.y);  
+    }
+
+}
